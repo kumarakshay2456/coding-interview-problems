28d4b4a3fb415b70ceab339285849a7f6de33933
Akshay Kumar <akshay.kumar1@betterplace.co.in>
2024-08-01 01:30:15 +0530

    added the another example

--- arrays/rotate_matrix.py
+++ arrays/rotate_matrix.py
@@ -5,6 +5,8 @@ You have to rotate the image in-place, which means you have to modify the input 
 DO NOT allocate another 2D matrix and do the rotation.
 Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [[7,4,1],[8,5,2],[9,6,3]]
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 """
 
 from typing import List
@@ -43,3 +45,4 @@ class RotateMatrix:
 
 if __name__ == "__main__":
     RotateMatrix().rotate_matrix([[1,2,3],[4,5,6],[7,8,9]])
+    RotateMatrix().rotate_matrix([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
