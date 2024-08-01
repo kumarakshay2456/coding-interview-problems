8fccce8272f4f01fa77aebe635fc1c1ebeab27a2
Akshay Kumar <akshay.kumar1@betterplace.co.in>
2024-08-01 12:42:52 +0530

    added the another example

--- arrays/rotate_matrix.py
+++ arrays/rotate_matrix.py
@@ -46,3 +46,4 @@ class RotateMatrix:
 if __name__ == "__main__":
     RotateMatrix().rotate_matrix([[1,2,3],[4,5,6],[7,8,9]])
     RotateMatrix().rotate_matrix([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
+    RotateMatrix().rotate_matrix([[5,1,9,11,1],[2,4,8,10,2],[13,3,6,7,3],[15,14,12,16,4], [18,12,12,16,18]])
