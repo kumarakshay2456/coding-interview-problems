d27703d45f75f97d2ac658302454811febc80dc7
Akshay Kumar <kumarakshay2456@gmail.com>
2025-05-05 18:39:18 +0530

    handle the corner case

--- arrays/shortest_unsorted_continuous_subarray.py
+++ arrays/shortest_unsorted_continuous_subarray.py
@@ -17,18 +17,23 @@ def find_unsorted_array_length(arr):
     So, we can use:
         •	a left-to-right pass to find the last index where an element is smaller than the max so far → end
         •	a right-to-left pass to find the first index where an element is greater than the min so far → start
+
+    Why start -1 and end -2 becuase for this sorted array and this will not change -> end - start + 1 = (-2) - (-1) + 1 = -2 + 1 + 1 = 0
+    Case           start	end	end - start + 1	     Output
+    Already sorted	-1	    -2	           0	     0
+    Unsorted region found	updated	updated	length of subarray	> 0
     """
 
     length = len(arr)
     maximum_so_far = arr[0]
-    minimum_so_far = arr[0]
-    end = 0
-    start = 0
+    minimum_so_far = arr[-1]
+    end = -2
+    start = -1
     for i in range(1, length):
         maximum_so_far = max(maximum_so_far, arr[i])
         if arr[i] < maximum_so_far:
             end = i
-    for i in range(length-1, 0, -1):
+    for i in range(length-2, -1, -1):
         minimum_so_far = min(minimum_so_far, arr[i])
         if arr[i] > minimum_so_far:
             start = i
@@ -62,5 +67,9 @@ if __name__ == '__main__':
     """
 
     arr = [2,6,4,8,10,9,15]
+    print("Answer is -> ", find_unsorted_array_length(arr))
+    arr = [5,4,3,2,1]
+    print("Answer is -> ", find_unsorted_array_length(arr))
+    arr = [1,1,1]
     print("Answer is -> ", find_unsorted_array_length(arr))
 
