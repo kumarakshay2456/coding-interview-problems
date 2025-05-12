b867e1f0efe0a5d08f289cfd7248b358291fa099
Akshay Kumar <kumarakshay2456@gmail.com>
2025-05-12 23:52:54 +0530

    solved the merge array problem

--- arrays/merge_array.py
+++ arrays/merge_array.py
@@ -5,7 +5,7 @@ def merge(intervals):
     if not intervals:
         return []
 
-    # Step 1: Sort using a regular function instead of lambda
+    # Step 1: Sort 
     intervals.sort(key=get_start)
 
     merged = [intervals[0]]
@@ -21,5 +21,29 @@ def merge(intervals):
 
     return merged
 
+
+def merge_v2(intervals):
+    # Space complexcity is O(1)
+
+    if not intervals:
+        return []
+
+    intervals.sort(key=get_start)
+
+    index = 0  # Points to the last merged interval
+
+    for i in range(1, len(intervals)):
+        current = intervals[i]
+        last = intervals[index]
+
+        if current[0] <= last[1]:  # Overlap
+            last[1] = max(last[1], current[1])
+        else:
+            index += 1
+            intervals[index] = current  # Move non-overlapping interval forward
+
+    return intervals[:index + 1]
+
 if __name__ =='__main__':
     print(merge(intervals = [[1, 4], [2, 5], [7, 9]]))
+    print(merge_v2(intervals = [[1, 4], [2, 5], [7, 9]]))
