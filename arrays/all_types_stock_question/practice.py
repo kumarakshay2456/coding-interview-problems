6ff0183e1b4bd8c909fb5ddf40aba6f0bbed34f9
Akshay Kumar <kumarakshay2456@gmail.com>
2026-04-27 21:57:53 +0530

    added the questions and soln

--- arrays/all_types_stock_question/practice.py
+++ arrays/all_types_stock_question/practice.py
@@ -1,35 +1,30 @@
-get_square_sum_cache = {}
+"""
+Given a string s, return the length of the longest palindrome that can be built with the letters of s.
 
-def get_square_sum(n:int):
-    if n in get_square_sum_cache:
-        return get_square_sum_cache[n]
-    if n == 1:
-        return 1
-    square_sum = 0
-    temp = n
-    while temp:
-        digit = temp % 10
-        temp = temp // 10
-        square_sum += digit*digit
-    get_square_sum_cache[n] = square_sum
-    return square_sum
+Letters are case-sensitive, so "Aa" is not the same as "aa"
+
+Ex - s = "abccccdd"
+Output - 7
 
 
-def is_number_is_happy(n:int):
-    if n == 1:
-        return True
-    slow = n
-    fast = get_square_sum(n)
-    while fast != 1 and slow != fast:
-        slow = get_square_sum(slow)
-        fast = get_square_sum(get_square_sum(fast))
-        print(f"slow {slow} and fast {fast}")
-    return fast == 1
+Ex2 - s = "bananas"
+Output - 5
 
+"""
+from collections import Counter
+def longest_palindrome(chars):
+    data_dicts = Counter(chars)
+    count = 0
+    odd_counter = False
+    for value in data_dicts.values():
+        if value % 2 == 0:
+            count += value
+        else:
+            count += value - 1 
+            odd_counter = True
+    
+    return count + 1 if odd_counter else count
 
-
-def main():
-    num = 19
-    print(is_number_is_happy(num))
 if __name__ == "__main__":
-    main()
+    chars = "bananas"
+    print(longest_palindrome(chars))
